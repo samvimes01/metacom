9e98121e48167fd2317e7488a89ff5f9d3ca772f
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2026-01-08 16:57:59 +0200

    Rm callId, fix metacom singleton inside proxy

--- metacom-service-worker.js
+++ metacom-service-worker.js
@@ -4,6 +4,8 @@ importScripts('metacom.iife.js');
 // exists in global scope after importScripts
 const { Metacom } = metacomIIFE;
 
+const metacomUrls = new Map();
+
 self.addEventListener('message', async ({ data, ports } = {}) => {
   if (!ports[0] || !data || data.type !== 'PORT_INITIALIZATION') {
     return;
@@ -11,12 +13,17 @@ self.addEventListener('message', async ({ data, ports } = {}) => {
   const messagePort = ports[0];
   const { url, metacomLoad } = data;
 
-  const metacom = Metacom.create(url);
+  let metacom = metacomUrls.get(url);
+
+  if (!metacom) {
+    metacom = Metacom.create(url);
+    metacomUrls.set(url, metacom);
+  }
 
   messagePort.onmessage = async ({ data }) => {
     const { unit, method, packet } = data;
     const { id, args } = packet;
-    const result = await metacom.api[unit][method](args, id);
+    const result = await metacom.api[unit][method](args);
     messagePort.postMessage({
       type: 'RESULT',
       payload: {
